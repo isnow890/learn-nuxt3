d48c1c78e2a5990ea2dbfef9226a2de37fc23245
isnow890 <isnow890@hotmail.com>
2024-06-11 00:26:00 +0900

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+
+  typescript: {
+    shim: false,
+  },
+
+  modules: ['nuxt-quasar-ui'],
+  quasar: {
+    /* */
+  },
+});
